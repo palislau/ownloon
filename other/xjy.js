a87d85a4af8e6172be278ddc98e19c9cdac7ede1
palislau <35599346+palislau@users.noreply.github.com>
2021-06-30 11:16:38 +0800

    Update xjy.js

--- other/xjy.js
+++ other/xjy.js
@@ -1,1 +1,5 @@
-aa
+$request.headers["X-AUTH-TOKEN"]="eyJhbGciOiJIUzUxMiIsImlhdCI6MTYxNzYxODU3MywiZXhwIjoxOTMyOTc4NTczfQ.eyJpZCI6NDEwNDEyMjl9.Jfxjcd2-FQKt4HYXxFte_NY_Rhcgij_f6g-BxyfHlennQc_CCbMbvXAVMUYp8CrzVDtwKc0KD0ejaO1-kRrhEg$2
+";
+//let obj = JSON.parse($response.body);
+//obj.is_forever = true;
+$done();
