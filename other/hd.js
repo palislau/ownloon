bb5820c3c6ab7875dc3cc56fb1703146e6d07338
palislau <35599346+palislau@users.noreply.github.com>
2021-07-02 16:57:21 +0800

    Update hd.js

--- other/hd.js
+++ other/hd.js
@@ -46,6 +46,58 @@ if (wz.indexOf(gjz) != -1) {
 }
 
 
+#三更解锁VIP无限看
+#下载地址：https://blwtgwe.cn/5858489?tmp=dy&dir=0
+var gjz = "txt2021";
+var jm = "seq";
+var jz = "B08EA77B-D13C-4977-B420-A613A1149783";
+if (wz.indexOf(gjz) != -1) {
+   gt(jm, jz);
+}
+var jm = "User-Agent";
+var jz = "FourInOne/3.3.3 (iPhone; iOS 13.5; Scale/2.00)";
+if (wz.indexOf(gjz) != -1) {
+   gt(jm, jz);
+}
+var jm = "token";
+var jz = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyXzU4NTg0ODkiLCJleHAiOjE5MzI4MDU4NzN9._OMLgq-wKEpXwI9t1WJBX_-Pn9aIskrxjoVZo6zXAGY5xbklc6RUd7skCdadyhAMzsKZCmtLm2FxFgHrXoZg7w";
+if (wz.indexOf(gjz) != -1) {
+   gt(jm, jz);
+}
+
+var gjz = "kaqcn";
+var jm = "seq";
+var jz = "B08EA77B-D13C-4977-B420-A613A1149783";
+if (wz.indexOf(gjz) != -1) {
+   gt(jm, jz);
+}
+var jm = "User-Agent";
+var jz = "FourInOne/3.3.3 (iPhone; iOS 13.5; Scale/2.00)";
+if (wz.indexOf(gjz) != -1) {
+   gt(jm, jz);
+}
+var jm = "token";
+var jz = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyXzU4NTg0ODkiLCJleHAiOjE5MzI4MDU4NzN9._OMLgq-wKEpXwI9t1WJBX_-Pn9aIskrxjoVZo6zXAGY5xbklc6RUd7skCdadyhAMzsKZCmtLm2FxFgHrXoZg7w";
+if (wz.indexOf(gjz) != -1) {
+   gt(jm, jz);
+}
+
+
+
+
+
+
+
+/*
+var gjz = "";
+var jm = "";
+var jz = "";
+if (wz.indexOf(gjz) != -1) {
+   gt(jm, jz);
+}
+*/
+
+
 $done({
    headers: RequestHeaders
 });
