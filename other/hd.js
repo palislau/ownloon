ab610f638773eaead8416e92a959331e6b832981
palislau <35599346+palislau@users.noreply.github.com>
2021-07-02 15:22:41 +0800

    Update hd.js

--- other/hd.js
+++ other/hd.js
@@ -1,33 +1,53 @@
 /*
-Keep app unlock private lessons and action libraries.
-There is a bug in the QX app. This script may not work, but the surge does not have this problem.
+轻颜相机 Ulike 蒸汽波相机(vaporcam) 三合一解锁VIP
+https://(commerce-.*api|pay).(faceu|wecut).(com|mobi)/(commerce|apple)/(iosAppVerifyReceipt.php|v1/subscription/user_info) url script-response-body nzw9314/Script/qyxj.js
+hostname= commerce-i18n-api.faceu.mobi,commerce-api.faceu.mobi, pay.wecut.com
 
-QX1.0.0:
-^https:\/\/api\.gotokeep\.com\/(.+\/subject|.+\/dynamic) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Keep.js
-
-Surge4.0:
-http-response https:\/\/api\.gotokeep\.com\/(.+\/subject|.+\/dynamic) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Keep.js
-
-MITM = api.gotokeep.com
-*/
-
-/*
-let url = $request.url;
-let body = $response.body;
-let obj = JSON.parse(body);
-
-const path1 = 'dynamic';
-const path2 = 'subject';
-
-if (url.indexOf(path1) != -1) {
-   obj.data.permission.isMembership = true;
-   obj.data.permission.membership = true;
-   obj.data.permission.inSuit = true;
+const path1 = "/commerce/v1/subscription/user_info";
+const path2 = "/apple/iosAppVerifyReceipt.php";
+let obj = JSON.parse($response.body);
+if ($request.url.indexOf(path1) != -1) {
+   obj.data.start_time = 1584674770;
+   obj.data.end_time = 4077660370;
+   obj.data.is_cancel_subscribe = true;
+   obj.data.flag = true;
 }
-if (url.indexOf(path2) != -1) {
-   for (var i = 0; i < obj.data.subjectInfos.length; i++) {
-         obj.data.subjectInfos[i].needPay = false;
+if ($request.url.indexOf(path2) != -1) {
+   obj.data = {
+      "isValid": 1,
+      "expiresTs": 4077660370
    }
 }
-body = JSON.stringify(obj);
-$done({body});
+$done({
+   body: JSON.stringify(obj)
+});
+if ($request) gt();
+
+*/
+
+var wz = $request.url;
+var RequestHeaders = $request.headers;
+
+
+//各个以下
+
+//知音视频解锁VIP无限看（by 公众号 iOS黑科技）
+//下载地址
+//https://sghoxm.xyz?p=WD44PJ
+var gjz = "apic";
+var jm = "Authorization";
+var jz = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MjIzODAzIiwiaXNzIjoiIiwiaWF0IjoxNjE4NjQwMDY3LCJuYmYiOjE2MTg2NDAwNjcsImV4cCI6MTc3NjMyMDA2N30.OA8xpC_bd7epGV5dp-Cr9aNfAiLwCkEKX_qpepcdBZY$2";
+if (wz.indexOf(gjz) != -1) {
+   gt(jm, jz);
+}
+
+
+$done({
+   headers: RequestHeaders
+});
+
+
+function gt(key, keyval) {
+   RequestHeaders[key] = keyval;
+   console.log(RequestHeaders + "改写headers成功");
+}
