215d5c27bb9ed8840b345480303e70f503c1462d
palislau <35599346+palislau@users.noreply.github.com>
2021-07-02 17:50:32 +0800

    Update hd.js

--- other/hd.js
+++ other/hd.js
@@ -1,30 +1,3 @@
-/*
-轻颜相机 Ulike 蒸汽波相机(vaporcam) 三合一解锁VIP
-https://(commerce-.*api|pay).(faceu|wecut).(com|mobi)/(commerce|apple)/(iosAppVerifyReceipt.php|v1/subscription/user_info) url script-response-body nzw9314/Script/qyxj.js
-hostname= commerce-i18n-api.faceu.mobi,commerce-api.faceu.mobi, pay.wecut.com
-
-const path1 = "/commerce/v1/subscription/user_info";
-const path2 = "/apple/iosAppVerifyReceipt.php";
-let obj = JSON.parse($response.body);
-if ($request.url.indexOf(path1) != -1) {
-   obj.data.start_time = 1584674770;
-   obj.data.end_time = 4077660370;
-   obj.data.is_cancel_subscribe = true;
-   obj.data.flag = true;
-}
-if ($request.url.indexOf(path2) != -1) {
-   obj.data = {
-      "isValid": 1,
-      "expiresTs": 4077660370
-   }
-}
-$done({
-   body: JSON.stringify(obj)
-});
-if ($request) gt();
-
-*/
-
 var wz = $request.url;
 var RequestHeaders = $request.headers;
 
@@ -46,8 +19,8 @@ if (wz.indexOf(gjz) != -1) {
 }
 
 
-#三更解锁VIP无限看
-#下载地址：https://blwtgwe.cn/5858489?tmp=dy&dir=0
+// 三更解锁VIP无限看
+// 下载地址： https: //blwtgwe.cn/5858489?tmp=dy&dir=0
 var gjz = "txt2021";
 var jm = "seq";
 var jz = "B08EA77B-D13C-4977-B420-A613A1149783";
@@ -81,10 +54,6 @@ var jz = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyXzU4NTg0ODkiLCJleHAiOjE5MzI4MDU4N
 if (wz.indexOf(gjz) != -1) {
    gt(jm, jz);
 }
-
-
-
-
 
 
 
