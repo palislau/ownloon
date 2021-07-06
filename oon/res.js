25f7dabbdc955506f0e2bcaddf79c3f3cb7899be
palislau <35599346+palislau@users.noreply.github.com>
2021-07-06 17:14:13 +0800

    Update res.js

--- oon/res.js
+++ oon/res.js
@@ -4,18 +4,21 @@ let obj = JSON.parse(body);
 
 //一Guan
 //cure情况一
-if (obj.data.cureNum != undefined) {
-   obj.data.cloverNum = 999;
-   obj.data.cureNum = 3;
-   obj.data.giftedCloverNum = 999;
-   obj.data.poisonNum = 3;
-} else
-   //cure情况二
-   if (obj.data.userProp.cureNum != undefined) {
-   obj.data.userProp.cloverNum = 999;
-   obj.data.userProp.cureNum = 3;
-   obj.data.userProp.giftedCloverNum = 999;
-   obj.data.userProp.poisonNum = 3;
+if (obj.hasOwnProperty("data")) {
+   if (obj.data.cureNum != undefined) {
+      obj.data.cloverNum = 999;
+      obj.data.cureNum = 3;
+      obj.data.giftedCloverNum = 999;
+      obj.data.poisonNum = 3;
+   } else
+   if (obj.data.hasOwnProperty("userProp")) {
+      if (obj.data.userProp.cureNum != undefined) {
+         obj.data.userProp.cloverNum = 999;
+         obj.data.userProp.cureNum = 3;
+         obj.data.userProp.giftedCloverNum = 999;
+         obj.data.userProp.poisonNum = 3;
+      }
+   }
 } else {
    obj.code = 0;
 }
