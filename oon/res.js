48e63778abeb50cc7b286e77380c0b764eb18cfb
palislau <35599346+palislau@users.noreply.github.com>
2021-07-06 16:39:09 +0800

    Update res.js

--- oon/res.js
+++ oon/res.js
@@ -1,9 +1,25 @@
-
-
 let body = $response.body;
-//console.log($response);
 let obj = JSON.parse(body);
 
-   obj.code = 0;
-$done({body: JSON.stringify(obj)});
 
+//一Guan
+//cure情况一
+if obj.data.cureNum != undefined {
+   obj.data.cloverNum = 999;
+   obj.data.cureNum = 3;
+   obj.data.giftedCloverNum = 999;
+   obj.data.poisonNum = 3;
+} else
+   //cure情况二
+   if obj.data.userProp.cureNum != undefined {
+   obj.data.userProp.cloverNum = 999;
+   obj.data.userProp.cureNum = 3;
+   obj.data.userProp.giftedCloverNum = 999;
+   obj.data.userProp.poisonNum = 3;
+} else
+   obj.code = 0;
+
+
+$done({
+   body: JSON.stringify(obj)
+});
