276d9153de027f50ddcc0bd220dd37ca6512102c
palislau <35599346+palislau@users.noreply.github.com>
2021-07-06 16:51:27 +0800

    Update res.js

--- oon/res.js
+++ oon/res.js
@@ -4,14 +4,14 @@ let obj = JSON.parse(body);
 
 //一Guan
 //cure情况一
-if obj.data.cureNum != undefined {
+if (obj.data.cureNum != undefined) {
    obj.data.cloverNum = 999;
    obj.data.cureNum = 3;
    obj.data.giftedCloverNum = 999;
    obj.data.poisonNum = 3;
 } else
    //cure情况二
-   if obj.data.userProp.cureNum != undefined {
+   if (obj.data.userProp.cureNum != undefined) {
    obj.data.userProp.cloverNum = 999;
    obj.data.userProp.cureNum = 3;
    obj.data.userProp.giftedCloverNum = 999;
