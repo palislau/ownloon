ec342340811c46f32e126cda5ae9741603d118bd
palislau <35599346+palislau@users.noreply.github.com>
2021-07-07 11:47:51 +0800

    Update res.js

--- oon/res.js
+++ oon/res.js
@@ -1,26 +1,41 @@
 let body = $response.body;
 let obj = JSON.parse(body);
+var wz = $request.url;
 
-console.log($request);
+
 //一Guan
-//cure情况一
-if (obj.hasOwnProperty("data")) {
-   if (obj.data.cureNum != undefined) {
-      obj.data.cloverNum = 999;
-      obj.data.cureNum = 3;
-      obj.data.giftedCloverNum = 999;
-      obj.data.poisonNum = 3;
-   } else
-   if (obj.data.hasOwnProperty("userProp")) {
-      if (obj.data.userProp.cureNum != undefined) {
-         obj.data.userProp.cloverNum = 999;
-         obj.data.userProp.cureNum = 3;
-         obj.data.userProp.giftedCloverNum = 999;
-         obj.data.userProp.poisonNum = 3;
+var gjz = "api.jijigugu.club";
+if (wz.indexOf(gjz) != -1) {
+   if (obj.hasOwnProperty("data")) {
+      if (obj.data.cureNum != undefined) {
+         obj.data.cloverNum = 999;
+         obj.data.cureNum = 3;
+         obj.data.giftedCloverNum = 999;
+         obj.data.poisonNum = 3;
+      } else
+      if (obj.data.hasOwnProperty("userProp")) {
+         if (obj.data.userProp.cureNum != undefined) {
+            obj.data.userProp.cloverNum = 999;
+            obj.data.userProp.cureNum = 3;
+            obj.data.userProp.giftedCloverNum = 999;
+            obj.data.userProp.poisonNum = 3;
+         }
+      }
+   } else {
+      obj.code = 0;
+   }
+}
+
+
+
+//物语
+var gjz = "sapi.tiduhn.cn:443/wecoreapi";
+if (wz.indexOf(gjz) != -1) {
+   if (obj.hasOwnProperty("data")) {
+      if (obj.data.times != undefined) {
+         obj.data.times = 8;
       }
    }
-} else {
-   obj.code = 0;
 }
 
 
