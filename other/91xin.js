6162aabb2b1b4d2ae34842e09b4d15e437d9ccc7
palislau <35599346+palislau@users.noreply.github.com>
2021-07-22 14:55:37 +0800

    Create 91xin.js

---
+++ other/91xin.js
@@ -0,0 +1,10 @@
+var RequestHeaders = $request.headers;
+if (RequestHeaders.hasOwnProperty("X-AUTH-TOKEN")) {
+   var jz = "eyJhbGciOiJIUzUxMiIsImlhdCI6MTYxNzYxODU3MywiZXhwIjoxOTMyOTc4NTczfQ.eyJpZCI6NDEwNDEyMjl9.Jfxjcd2-FQKt4HYXxFte_NY_Rhcgij_f6g-BxyfHlennQc_CCbMbvXAVMUYp8CrzVDtwKc0KD0ejaO1-kRrhEg$2";
+   RequestHeaders["X-AUTH-TOKEN"] = jz;
+}
+$done({
+   headers: RequestHeaders
+});
+console.log(RequestHeaders);
+console.log("改写headers成功");
