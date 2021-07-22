dfb104b2391b6806db82e85e84734845c967c88e
palislau <35599346+palislau@users.noreply.github.com>
2021-07-22 15:22:38 +0800

    Update 91xin.js

--- other/91xin.js
+++ other/91xin.js
@@ -1,4 +1,6 @@
 var RequestHeaders = $request.headers;
+console.log("这是原始的");
+console.log(RequestHeaders);
 if (RequestHeaders.hasOwnProperty("X-AUTH-TOKEN")) {
    if (RequestHeaders["X-AUTH-TOKEN"]!=""){
    var jz = "eyJhbGciOiJIUzUxMiIsImlhdCI6MTYxNzYxODU3MywiZXhwIjoxOTMyOTc4NTczfQ.eyJpZCI6NDEwNDEyMjl9.Jfxjcd2-FQKt4HYXxFte_NY_Rhcgij_f6g-BxyfHlennQc_CCbMbvXAVMUYp8CrzVDtwKc0KD0ejaO1-kRrhEg$2";
